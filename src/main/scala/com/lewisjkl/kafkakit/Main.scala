468e8a8be4ebed4afd3f234dfce8ace5a07b553e
Jeff Lewis <lewisjkl@me.com>
2020-02-27 19:12:26 -0700

    complete allow changing kafka cluster on the fly

--- src/main/scala/com/lewisjkl/kafkakit/Main.scala
+++ src/main/scala/com/lewisjkl/kafkakit/Main.scala
@@ -51,7 +51,7 @@ object Choice {
       Opts.subcommand("consume", "Consume records from a topic") (
         (topicNameArg, limitOption, tailFlag, clusterOption).mapN(ConsumeTopic)
       )
-    ).reduceK orElse(clusterOption)
+    ).reduceK
 }
 
 object Main extends CommandIOApp(
@@ -63,32 +63,33 @@ object Main extends CommandIOApp(
   case object KafkaClusterNotFound extends NoStackTrace
 
   private def runApp[F[_]: Sync: KafkaProgram: MonadState[*[_], KafkaCluster]](config: Config): Choice => F[Unit] = {
-    val changeCluster: String => F[Unit] = { newClusterNickname =>
-      config.kafkaClusters.find(_.nickname === newClusterNickname) match {
+    val changeCluster: Option[String] => F[Unit] = _.map { newClusterNickname =>
+      val maybeSetNewCluster: F[Unit] = config.kafkaClusters.find(_.nickname === newClusterNickname) match {
         case Some(newCluster) => MonadState[F, KafkaCluster].set(newCluster)
         case None => Sync[F].raiseError(KafkaClusterNotFound)
       }
-    }
+      maybeSetNewCluster
+    }.getOrElse(Sync[F].unit)
 
     {
       case Choice.ListTopics(altCluster) => changeCluster(altCluster) *> KafkaProgram[F].listTopics
-      case Choice.ConsumeTopic(topicName, limit, tail, altCluster) => KafkaProgram[F].consume(topicName, limit, tail).compile.drain
+      case Choice.ConsumeTopic(topicName, limit, tail, altCluster) => changeCluster(altCluster) *>
+        KafkaProgram[F].consume(topicName, limit, tail).compile.drain
       case _ => Sync[F].unit
     }
   }
 
   val makeProgram: Resource[IO, Choice => IO[Unit]] =
-    BootstrapProgram.makeConfigLoader[IO].map { implicit configLoader =>
+    BootstrapProgram.makeConfigLoader[IO].flatMap { configLoader =>
       val kafkaProgram = for {
         config <- configLoader.load
         ref <- Ref[IO].of(config.defaultCluster)
         kafka <- ref.runState { implicit monadState =>
-          IO(KafkaClient.live[IO])
+          implicit val kafkaProgram: KafkaProgram[IO] = KafkaProgram.live[IO](KafkaClient.live[IO])
+          IO(runApp[IO](config))
         }
-      } yield {
-        implicit val kafkaProgram: KafkaProgram[IO] = KafkaProgram.live[IO](kafka)
-        runApp[IO]
-      }
+      } yield kafka
+      Resource.liftF(kafkaProgram)
     }
 
   val mainOpts: Opts[IO[Unit]] = Choice
